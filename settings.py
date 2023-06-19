05d7045a28b153b84f56981874b932779eab49ca
IX <borntokill.ix@gmail.com>
2023-06-19 20:40:54 +0300

    Create a logging config file. Rewrite the watcher for new logging form.
    stash

---
+++ settings.py
@@ -0,0 +1,32 @@
+LogConfig = {
+    'version': 1,
+    'formatters': {
+        'details': {
+            'class': 'logging.Formatter',
+            'format': '%(asctime)s::%(name)s::%(levelname)s::%(message)s',
+            'incremental': True,
+        },
+    },
+    'handlers': {
+        'rotate': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': 'wg.log',
+            'mode': 'w',
+            'level': 'DEBUG',
+            'maxBytes': 204800,
+            'backupCount': 3,
+            'formatter': 'details',
+        },
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'DEBUG',
+            'formatter': 'details',
+        },
+    },
+    'loggers': {
+        'root': {
+            'level': 'NOTSET',
+            'handlers': ['rotate', 'console'],
+        },
+    }
+}
